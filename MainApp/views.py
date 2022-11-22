9fda1a6f0684617b2f3dc13b0e1d12b104777102
A-Erokhin <a-erkhin@yandex.ru>
2022-11-22 09:51:44 +0300

    DZ

---
+++ MainApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import Http404
+from countries_dict import countries
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def countries_list(request):
+    context = {
+        "countries": countries
+    }
+    return render(request, 'countries-list.html', context)
+
+def country_page(request, name):
+    for this_country in countries:
+        context = {}
+        if this_country['country'] == name:
+            context = {
+                "this_country": this_country
+            }
+        return render(request, 'country_page.html', context)
+    raise Http404(f"Страна {name} не найдена")
